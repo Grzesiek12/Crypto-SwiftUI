f1e6d74f6230ea80ff8e3dfb728acb656ca50056
Grzesiek12 <grz.wiczk@gmail.com>
2023-02-21 13:29:22 +0100

    Initial Commit

---
+++ Crypto SwiftUI/Crypto_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_SwiftUIApp.swift
+//  Crypto SwiftUI
+//
+//  Created by Grzegorz Wiczkowski on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
