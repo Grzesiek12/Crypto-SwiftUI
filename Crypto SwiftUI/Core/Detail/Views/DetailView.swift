ffda99bcf228e1d2b992087050bd44cedfce061e
Grzesiek12 <grz.wiczk@gmail.com>
2023-02-24 12:19:38 +0100

    Coin -> DetailView

---
+++ Crypto SwiftUI/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,42 @@
+//
+//  DetailView.swift
+//  Crypto SwiftUI
+//
+//  Created by Grzegorz Wiczkowski on 24/02/2023.
+//
+
+import SwiftUI
+
+struct DetailLoadingView: View {
+    
+    @Binding var coin: CoinModel?
+    
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        print("Init Detail View for \(coin.name)")
+    }
+    
+    var body: some View {
+        
+        Text(coin.name)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(coin: dev.coin)
+    }
+}
